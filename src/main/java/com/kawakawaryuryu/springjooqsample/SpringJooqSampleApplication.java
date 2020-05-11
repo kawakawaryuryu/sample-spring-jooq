57fed1a9f843d98e3d8b04f26889330e3ef2c369
Ryutaro Kawaguchi <kawakawaryuryu@hotmail.co.jp>
2020-05-11 22:50:04 +0900

    initial commit

---
+++ src/main/java/com/kawakawaryuryu/springjooqsample/SpringJooqSampleApplication.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.springjooqsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJooqSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJooqSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/kawakawaryuryu/springjooqsample/SpringJooqSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kawakawaryuryu.springjooqsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJooqSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
